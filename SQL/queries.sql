62fb61771a8f39de4e7ebc26aaae1bc7cc416df0
Donnell Jackson <55963911+DonnieData@users.noreply.github.com>
2024-01-29 18:08:07 -0600

    Update queries.sql

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -1,3 +1,10 @@
+select * from orders ord
+    inner join lineitem lit
+        on ord.o_orderkey = lit.l_orderkey
+    inner join part 
+    on part.p_partkey = lit.l_partkey
+    where month(ord.o_orderdate) between 1 and 2;
+
 select 
 query_id
 ,start_time
